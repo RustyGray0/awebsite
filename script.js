1c8a3d34ff6973549293e95ffc551e31573895e8
RustyGray0 <abhi.chulani@gmail.com>
2022-09-14 09:24:49 -0400

    a

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const splash = document.querySelector("#splash");
-
 document.addEventListener("DOMContentLoaded", (e)=>{
     setTimeout(()=>{
         document.getElementById("splash").style.opacity = "0";
         document.getElementById("splash").style.zIndex = "-100";
-        document.getElementById("splash").style.transition = "all .7s";
+        document.getElementById("splash").style.transition = "all .5";
     }, 2000);
 })
